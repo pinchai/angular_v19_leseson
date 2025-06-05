f4526c50ae4c32a6f32eeaa854e01a16aa7c1b03
Pinchai <>
2025-06-05 11:11:49 +0700

    com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
+
+interface student {
+  id: number,
+  name:string,
+  age:number,
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    NgFor
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = ''
-  age = 0
+  student_list: student [] = []
+  name_student: string = '';
+  age:number = 0;
+
+  changeData() {
+    this.student_list.push(
+      {
+        id: 1,
+        name: this.name_student,
+        age: this.age
+      },
+    )
+
+    //clearForm
+    this.clearForm()
+  }
+
+  clearForm(){
+    this.name_student = ''
+    this.age = 0
+  }
 }
