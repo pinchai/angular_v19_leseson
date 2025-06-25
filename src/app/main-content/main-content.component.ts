8ee6c94986f09747c916d4301635863c0ae6b7fc
Pinchai <>
2025-06-25 09:59:40 +0700

    add more lesson

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
 export class MainContentComponent {
+  image: string = 'mis.jpg';
+  age: number = 17;
 
+  changeImage(): void {
+    if (this.image === 'mis.jpg') {
+      this.image = 'mikrotik.jpg';
+    } else {
+      this.image = 'mis.jpg';
+    }
+  }
+
+  viewDetail(): void {
+    alert('Detail');
+  }
+
+
+  increaseAge(){
+    this.age++;
+  }
+  decreaseAge(){
+    this.age--;
+  }
 }
